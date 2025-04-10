ecdcbe939a1c8008668504a4f3914a5c29509c2d
Sazzadhosen10 <hosen15-5836@diu.edu.bd>
2025-04-11 03:25:05 +0600

    firebasesetup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:mediq/homepage.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -12,12 +18,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        
-      ),
+     
+      title: 'Flutter ',
+      theme: ThemeData(),
       home: Homepage(),
     );
   }
 }
-
