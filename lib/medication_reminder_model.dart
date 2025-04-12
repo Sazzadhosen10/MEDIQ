7c3b5c1eee213671c1c83902629191b152181e36
atef5922 <sifat15-5922@diu.edu.bd>
2025-04-13 03:43:30 +0600

    Medication_Reminder_Process

--- lib/medication_reminder_model.dart
+++ lib/medication_reminder_model.dart
@@ -1,5 +1,6 @@
 class MedicationReminder {
   final String id;
+  final String userId;
   final String name;
   final String dosage;
   final int hour;
@@ -8,16 +9,18 @@ class MedicationReminder {
 
   MedicationReminder({
     required this.id,
+    required this.userId,
     required this.name,
     required this.dosage,
     required this.hour,
     required this.minute,
-    this.isActive = true,
+    required this.isActive,
   });
 
   Map<String, dynamic> toMap() {
     return {
       'id': id,
+      'userId': userId,
       'name': name,
       'dosage': dosage,
       'hour': hour,
@@ -29,6 +32,7 @@ class MedicationReminder {
   factory MedicationReminder.fromMap(Map<String, dynamic> map) {
     return MedicationReminder(
       id: map['id'],
+      userId: map['userId'] ?? '',
       name: map['name'],
       dosage: map['dosage'],
       hour: map['hour'],
